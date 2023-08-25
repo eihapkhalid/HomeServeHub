b341671a2e606a1d0fa4ac4fba82ef8f18b87251
Eihap Khalid <Eihapinfo@gmail.com>
2023-08-25 12:47:53 +0300

    Create AppointmentDTO and AppointmentController

---
+++ HomeServeHub.Models/DTO/AppointmentDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HomeServeHub.Models.DTO
+{
+    public class AppointmentDTO
+    {
+        [Key]
+        public int AppointmentID { get; set; }
+
+        [Required(ErrorMessage = "حقل تاريخ ووقت الحجز مطلوب.")]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "تاريخ ووقت الحجز")]
+        public DateTime AppointmentDateTime { get; set; }
+
+        //Payment method? cash, credit card, etc.
+        [Required(ErrorMessage = "حقل حالة الحجز مطلوب.")]
+        [Display(Name = "حالة الحجز")]
+        public string AppointmentStatus { get; set; }
+
+        //Active ? or not !
+        [Required(ErrorMessage = "حقل الحالة الحالية للحجز مطلوب.")]
+        public int AppointmentCurrentState { get; set; }
+
+        /**************************************************/
+
+        [Required(ErrorMessage = "حقل معرّف المستخدم مطلوب.")]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "حقل معرّف مقدم الخدمة مطلوب.")]
+        public int ServiceProviderID { get; set; }
+
+        [Required(ErrorMessage = "حقل معرّف الخدمة مطلوب.")]
+        public int ServiceID { get; set; }
+    }
+}
---
+++ HomeServeHub/Controllers/AppointmentController.cs
@@ -0,0 +1,153 @@
+﻿using HomeServeHub.DataAccess.UnitOfWork;
+using HomeServeHub.Models.DTO;
+using HomeServeHub.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeServeHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        #region Dependency Injection
+        private readonly IUnitOfWork _unitOfWork;
+        public AppointmentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        #endregion
+
+        #region GET All Appointments: api/<TransController>/Get
+        [HttpGet]
+        [Route("GetAllAppointments")]
+        public IActionResult GetAllAppointments()
+        {
+            var Appointments = _unitOfWork.TbAppointment.GetAll().ToList();
+            if (Appointments == null || Appointments.Count == 0)
+            {
+                return NotFound();
+            }
+
+            var cleanedAppointments = new List<object>();
+
+            foreach (var appointment in Appointments)
+            {
+                // تنظيف البيانات قبل إرسالها إلى العرض
+                var cleanedappointment = new
+                {
+                    appointment.AppointmentID,
+                    appointment.AppointmentDateTime,
+                    appointment.AppointmentStatus,
+                    appointment.AppointmentCurrentState,
+                    appointment.UserID,
+                    appointment.ServiceProviderID,
+                    appointment.ServiceID
+                };
+
+                cleanedAppointments.Add(cleanedappointment);
+            }
+
+            return Ok(cleanedAppointments);
+        }
+
+
+        #endregion
+
+        #region GET appointment By Id: api/<TransController>/Get/5
+        [HttpGet("GetappointmentById/{id}")]
+        public IActionResult GetappointmentById(int id)
+        {
+            var appointment = _unitOfWork.TbAppointment.Get(s => s.AppointmentID == id);
+
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            // تنظيف البيانات قبل إرسالها إلى العرض
+            var cleanedappointment = new
+            {
+                appointment.AppointmentID,
+                appointment.AppointmentDateTime,
+                appointment.AppointmentStatus,
+                appointment.AppointmentCurrentState,
+                appointment.UserID,
+                appointment.ServiceProviderID,
+                appointment.ServiceID                
+            };
+
+            return Ok(cleanedappointment);
+        }
+        #endregion
+
+        #region POST New or Edit appointment: api/<TransController>
+        [HttpPost("Postappointment")]
+        public IActionResult Postappointment([FromBody] AppointmentDTO newappointmentDTO)
+        {
+            if (newappointmentDTO == null)
+            {
+                return BadRequest();
+            }
+
+            var existingappointment = _unitOfWork.TbAppointment.Get(u => u.AppointmentID == newappointmentDTO.AppointmentID);
+            if (existingappointment != null)
+            {
+                // تحديث البيانات إذا كان المستخدم موجودًا
+                existingappointment.AppointmentDateTime = newappointmentDTO.AppointmentDateTime;
+                existingappointment.AppointmentStatus = newappointmentDTO.AppointmentStatus;
+                existingappointment.AppointmentCurrentState = newappointmentDTO.AppointmentCurrentState;
+                existingappointment.UserID = newappointmentDTO.UserID;
+                existingappointment.ServiceProviderID = newappointmentDTO.ServiceProviderID;
+                existingappointment.ServiceID = newappointmentDTO.ServiceID;
+                _unitOfWork.TbAppointment.Update(existingappointment);
+            }
+            else
+            {
+                // إضافة المستخدم إذا كان غير موجود
+                var newappointment = new TbAppointment
+                {
+                    AppointmentDateTime = newappointmentDTO.AppointmentDateTime,
+                    AppointmentStatus = newappointmentDTO.AppointmentStatus,
+                    AppointmentCurrentState = newappointmentDTO.AppointmentCurrentState,
+                    UserID = newappointmentDTO.UserID,
+                    ServiceProviderID = newappointmentDTO.ServiceProviderID,
+                    ServiceID = newappointmentDTO.ServiceID
+                };
+                _unitOfWork.TbAppointment.Add(newappointment);
+            }
+
+            _unitOfWork.Save();
+
+            return Ok();
+        }
+
+
+        #endregion
+
+        #region POST Delte appointment: api/<TransController>/Delete
+        [HttpPost("Deleteappointment")]
+        public IActionResult Deleteappointment([FromBody] AppointmentDTO newappointmentDTO)
+        {
+            if (newappointmentDTO == null)
+            {
+                return BadRequest();
+            }
+            var newappointment = new TbAppointment
+            {
+                AppointmentID = newappointmentDTO.AppointmentID,
+                AppointmentDateTime = newappointmentDTO.AppointmentDateTime,
+                AppointmentStatus = newappointmentDTO.AppointmentStatus,
+                AppointmentCurrentState = newappointmentDTO.AppointmentCurrentState,
+                UserID = newappointmentDTO.UserID,
+                ServiceProviderID = newappointmentDTO.ServiceProviderID,
+                ServiceID = newappointmentDTO.ServiceID
+            };
+            _unitOfWork.TbAppointment.Remove(newappointment);
+            _unitOfWork.Save();
+            return Ok();
+        }
+        #endregion
+    }
+}
